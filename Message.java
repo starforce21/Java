4bf01e4cad169a8be9960c5011def06251a5da4c
starforce21 <jackjack1214@gmail.com>
2022-02-03 10:08:47 -0500

    first commit

---
+++ Message.java
@@ -0,0 +1,14 @@
+class Message{
+    public static void main(String agrs[]){
+        int a,b,c;
+        a=1;
+        b=2;
+        System.out.println(a);
+        System.out.println(b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
